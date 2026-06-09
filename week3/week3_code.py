4f66f5862d4f1c71383c205d02dc3778661805f6
leannemc5 <leanne.mcmanus2@mail.dcu.ie>
2026-06-09 22:26:05 +0000

    Test for counting photon count

--- week3/week3_code.py
+++ week3/week3_code.py
@@ -2,8 +2,16 @@
 h = 6.626e-34
 c = 3e8
 
+#fake flux
+flux = 1e-9
+exposure_time = 10
+
 lambda_eff = np.array([400e-9, 550e-9, 700e-9])
 
 #calculate photon energy
 photon_energy = (h*c)/ lambda_eff
-print(photon_energy)
+
+photon_counts = (
+    flux*exposure_time
+)/photon_energy
+print(photon_counts)
