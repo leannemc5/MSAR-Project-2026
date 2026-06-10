24d8d9d75718d7c3402aec0b48c8a9b665c09552
leannemc5 <leanne.mcmanus2@mail.dcu.ie>
2026-06-10 11:35:13 +0000

    How a hot vs cold star is effected by filter

--- week4/toy_gaussian_model.py
+++ week4/toy_gaussian_model.py
@@ -1,9 +1,29 @@
+#needed libraires
 import numpy as np
 import matplotlib.pyplot as plt
 
-wavelengths_nm = np.linspace(100, 3000, 5000)
+#constants
+h = 6.626e-34
+c = 3.0e8
+k = 1.381e-23
 
-#toy gaussian model
+#plancks function
+def planck(wavelength, temperature):
+
+    exponent = (h*c)/(wavelength*k*temperature)
+
+    numerator = 2*h*c**2
+
+    denominator = (
+        wavelength**5 *
+        (np.exp(exponent)-1)
+    )
+
+    intensity = numerator / denominator
+
+    return intensity
+
+#toy gaussian
 def gaussian_filter(wavelengths, centre, width):
 
     response = np.exp(
@@ -14,17 +34,29 @@ def gaussian_filter(wavelengths, centre, width):
 
     return response
 
-#create toy filter
+wavelengths = np.linspace(
+    100e-9,
+    3000e-9,
+    5000
+)
+
+wavelengths_nm = wavelengths * 1e9
+
+#spectra
+cool_star = planck(wavelengths, 3000)
+
+hot_star = planck(wavelengths, 10000)
+
+#normalised
+cool_star = cool_star / np.max(cool_star)
+
+hot_star = hot_star / np.max(hot_star)
+
+#toy filters
 blue_filter = gaussian_filter(
     wavelengths_nm,
     450,
     50
-)
-
-green_filter = gaussian_filter(
-    wavelengths_nm,
-    550,
-    60
 )
 
 red_filter = gaussian_filter(
@@ -33,22 +65,44 @@ red_filter = gaussian_filter(
     80
 )
 
-plt.figure(figsize=(10,6))
+#add filters to spectra
+cool_blue = cool_star * blue_filter
+cool_red = cool_star * red_filter
+hot_blue = hot_star * blue_filter
+hot_red = hot_star * red_filter
 
-plt.plot(wavelengths_nm, blue_filter, label="Blue Filter")
+#plot
+plt.figure(figsize=(12,7))
 
-plt.plot(wavelengths_nm, green_filter, label="Green Filter")
+plt.plot(
+    wavelengths_nm,
+    cool_star,
+    label="Cool Star (3000 K)"
+)
 
-plt.plot(wavelengths_nm, red_filter, label="Red Filter")
+plt.plot(
+    wavelengths_nm,
+    hot_star,
+    label="Hot Star (10000 K)"
+)
+
+plt.plot(
+    wavelengths_nm,
+    blue_filter,
+    '--',
+    label="Blue Filter"
+)
+
+plt.plot(
+    wavelengths_nm,
+    red_filter,
+    '--',
+    label="Red Filter"
+)
 
 plt.xlabel("Wavelength (nm)")
-
-plt.ylabel("Transmission")
-
-plt.title("Simplified Photometric Filter Responses")
-
+plt.ylabel("Normalized Intensity")
+plt.title("Stellar Spectra with Filter Responses")
 plt.legend()
-
 plt.grid(True)
-
 plt.show()
