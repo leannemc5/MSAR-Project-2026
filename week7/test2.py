b8c5154ea8ba776bb9b38c687d5c892a3026ec93
leannemc5 <leanne.mcmanus2@mail.dcu.ie>
2026-08-14 19:05:38 +0000

    constant extinction

---
+++ week7/test2.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# constants
+h = 6.626e-34
+c = 3.0e8
+kB = 1.381e-23
+
+# planck function
+def planck(wavelength, T):
+
+    exponent = (h*c / (wavelength*kB*T))
+
+    intensity = ((2*h*c**2) / (wavelength**5 * (np.exp(exponent)-1)))
+
+    return intensity
+
+# wavelength grid
+wavelengths_nm = np.linspace(300, 1000, 5000)
+wavelengths_m = wavelengths_nm * 1e-9
+
+# blackbody
+T = 6000
+spectrum = planck(wavelengths_m, T)
+spectrum /= np.max(spectrum)
+
+# constant extinction
+airmass = 2.0
+k_ext = 0.2
+
+transmission = np.exp(-k_ext * airmass)
+
+extincted_spectrum = spectrum * transmission
+
+# plot
+plt.figure(figsize=(10,6))
+plt.plot(wavelengths_nm, spectrum, label="Original")
+plt.plot(wavelengths_nm, extincted_spectrum, label="After Extinction")
+
+plt.xlabel("Wavelength (nm)")
+plt.ylabel("Normalised Intensity")
+plt.title("Constant Atmospheric Extinction")
+plt.legend()
+plt.grid(True)
+plt.show()
